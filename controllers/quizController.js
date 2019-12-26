49039745a532e7df839e6547550e11318659fb48
Dhruv <dhruv479@gmail.com>
2019-12-26 18:01:11 +0530

    result calculation added

--- controllers/quizController.js
+++ controllers/quizController.js
@@ -1,5 +1,6 @@
 const Quiz = require('./../models/quiz');
 const Vote = require('./../models/voting');
+const mongoose = require('mongoose');
 
 module.exports = {
   addQuiz: (req, res) => {
@@ -24,18 +25,18 @@ module.exports = {
     Quiz.findById(req.params.quizId)
       .then(response => {
         let time = Number(new Date());
-        if (response.voteTime >= time) {
-          req.body.quizId = req.params.quizId;
-          let quizVotes = new Vote(req.body);
-          return quizVotes.save().then(() => {
-            return res.json({
-              error: false,
-              message: 'Votes Added Successfully'
-            });
+        // if (response.voteTime >= time) {
+        req.body.quizId = req.params.quizId;
+        let quizVotes = new Vote(req.body);
+        return quizVotes.save().then(() => {
+          return res.json({
+            error: false,
+            message: 'Votes Added Successfully'
           });
-        } else {
-          return res.json({ error: true, message: 'Voting Time Finished!' });
-        }
+        });
+        // } else {
+        //   return res.json({ error: true, message: 'Voting Time Finished!' });
+        // }
       })
       .catch(error => {
         console.log(error);
@@ -55,9 +56,60 @@ module.exports = {
             data: { resultTime: response.voteTime }
           });
         } else {
-          Vote.find({ quizId: req.params.quizId }).then(response => {});
+          Vote.find({ quizId: req.params.quizId })
+            .select('votes')
+            .then(response => {
+              let result = [
+                { a: 0, b: 0, c: 0, d: 0 },
+                { a: 0, b: 0, c: 0, d: 0 },
+                { a: 0, b: 0, c: 0, d: 0 },
+                { a: 0, b: 0 },
+                { a: 0, b: 0 }
+              ];
+              let totalVotes = 0;
+              response.map(voteObject => {
+                totalVotes += voteObject.votes.length;
+                result = getVotes(result, voteObject.votes);
+              });
+              percentResult = getPercentage(result, totalVotes);
+              return Quiz.findByIdAndUpdate(req.params.quizId, {
+                result: percentResult
+              }).then(() => {
+                return res.json({
+                  error: false,
+                  data: percentResult
+                });
+              });
+            })
+            .catch(error => {
+              console.log(error);
+              return res
+                .status(500)
+                .json({ error: true, message: 'Internal Error' });
+            });
         }
       }
     });
   }
 };
+
+function getVotes(result, votes) {
+  votes.map(userVote => {
+    for (question in userVote) {
+      result[Number(question) - 1][userVote[question]] += 1;
+    }
+  });
+  return result;
+}
+
+function getPercentage(result, totalVotes) {
+  console.log(result);
+  result.map((question, index) => {
+    for (option in question) {
+      result[index][option] = ((question[option] / totalVotes) * 100).toFixed(
+        2
+      );
+    }
+  });
+  return result;
+}
